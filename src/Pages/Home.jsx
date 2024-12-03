72c62e142eeeba92ffc59294dbb82faf7a29fbd1
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-12-03 18:29:59 +0530

    Added home page ui

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,30 @@
-import Footer from "Components/Footer/Footer";
+import logo from 'assets/Images/logo-color.png';
 
 function Home(){
     return(
-        <>
-            home <Footer/>
-        </>
+            <div className='flex flex-col items-center justify-center gap-20 h-[100vh]'>
+                <div className='h-48 w-48'>
+                    <img 
+                        className='w-full h-full'
+                        alt='logo'
+                        src={logo}
+                    />
+                </div>
+                <div className='flex justify-around items-center gap-16'>
+                    <div className='w-2/4 text-center font-semibold basis-1/2'>
+                        <h1 className='text-white text-5xl tracking-widest leading-normal'>
+                            BookShelf <br />
+                            <span className='text-warning'>
+                                Your personal library and social network for bookworms
+                            </span>
+                        </h1>
+                    </div>
+                    <div>
+                        <button className='btn btn-primary rounded-md px-5 py-2 text-xl text-white'>Register</button>
+                        <button className='btn btn-warning mx-3 rounded-md px-5 py-2 text-xl text-white'>Login</button>
+                    </div>
+                </div>
+            </div>  
     )
 }
 
