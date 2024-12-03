694c4c8b83e5480064911cfca5df3b223b7ba076
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-12-03 14:06:45 +0530

    Config absolute path for folder inside src folder

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import Footer from "Components/Footer/Footer";
+
+function Home(){
+    return(
+        <>
+            home <Footer/>
+        </>
+    )
+}
+
+export default Home;
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      Components: '/src/Components',
+      Pages: '/src/Pages',
+      assets: '/src/assets',
+      Configs: '/src/Configs',
+      Helpers: '/src/Helpers',
+      Layouts: '/src/Layouts',
+      Redux: '/src/Redux',
+      Routes: '/src/Routes'
+    }
+  }
 })
