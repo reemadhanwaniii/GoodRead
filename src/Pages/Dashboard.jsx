5500ae685ae4f431fa693fe1407ce993dc62f857
Reema Dhanwani <reemadhanwani1234@gmail.com>
2024-12-04 20:06:36 +0530

    Added bookcard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,7 +1,10 @@
 import Layout from "Layouts/Layout";
+import BookCard from "Components/BookCard/BookCard";
 
 export default function Dashboard() {
     return(
-        <Layout/>
+        <Layout>
+           
+        </Layout>
     );
 }
